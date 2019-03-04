0808e482460240dbe3313d727a75320c87373348
nikolabeerkens <48219661+nikolabeerkens@users.noreply.github.com>
2019-03-04 21:45:54 +0100

    Add files via upload

---
+++ Adulthood/main.cpp
@@ -0,0 +1,32 @@
+/* This program tests if a person has reached adulthood ( 18 years) and wheather they can run for president*/
+#include <iostream>
+
+using namespace std;
+
+int age;
+
+int main()
+{
+   cout <<"How old are you: ";
+   cin>>age;
+
+   if (age>=18)
+   {
+       cout <<"You have reached adulthood." <<endl;
+   }
+   else
+   {
+       cout <<"You have not reached adulthood. " <<endl;
+   }
+   if (wiek>=35)
+   {
+       cout <<"You can run for president."<<endl;
+   }
+   else
+   {
+       cout<<"You cannot run for president."<<endl;
+   }
+
+
+    return 0;
+}
